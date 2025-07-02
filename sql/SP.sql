9744da7a9a504e2eb937b88c57e5b7b0f9c875f8
subash poojary <subashpoojary@MacBook-Pro-16.local>
2025-07-01 20:08:52 -0400

    demo sp12

--- sql/SP.sql
+++ sql/SP.sql
@@ -1,15 +1,8 @@
 CREATE OR REPLACE PROCEDURE return_sample_data()
 RETURNS INTEGER  
 LANGUAGE SQL
-AS
-'
-SELECT 1
-;'
+AS 
+BEGIN
 
--- Example usage:
--- CALL return_sample_data();
--- This will return a table with sample data containing:
--- 1. An ID column (integer)
--- 2. A Name column (string)
--- 3. A Created timestamp
--- This will return a table with two null values in each row
+SELECT 1;
+END
