43fe2487329fb86c1f897cb6469b478442c6cfd6
subash poojary <subashpoojary@MacBook-Pro-16.local>
2025-07-09 16:59:47 -0500

    release tag4

--- Snowflake/SNOWFLAKE_LEARNING_DB/DATA_PIPELINE/Tables/Departments.sql
+++ Snowflake/SNOWFLAKE_LEARNING_DB/DATA_PIPELINE/Tables/Departments.sql
@@ -6,4 +6,5 @@ CREATE OR REPLACE TABLE Departments (
 INSERT INTO Departments (Department_ID, Department_Name) VALUES
 (101, 'Engineering'),
 (102, 'Marketing'),
-(103, 'Human Resources');
+(103, 'Human Resources'),
+(104, 'Finance');
