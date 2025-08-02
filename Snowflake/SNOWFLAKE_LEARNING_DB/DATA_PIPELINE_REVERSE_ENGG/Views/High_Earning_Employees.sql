5b32e14d076f382ed27c57408aa252707be52379
subash poojary <subashpoojary@macbook-pro-16.lan>
2025-08-01 21:43:07 -0400

    final release 1

--- Snowflake/SNOWFLAKE_LEARNING_DB/DATA_PIPELINE_REVERSE_ENGG/Views/High_Earning_Employees.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW High_Earning_Employees AS
-SELECT
-    e.Employee_ID,
-    e.Name,
-    s.Base_Salary + s.Bonus AS Total_Compensation
-FROM Employees e
-JOIN Salaries s ON e.Employee_ID = s.Employee_ID
-WHERE s.Base_Salary + s.Bonus > 100000;
